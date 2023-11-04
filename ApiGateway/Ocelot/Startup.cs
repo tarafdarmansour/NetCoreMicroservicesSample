7ee8a110abdb712025727ea7d7301c0a8f277204
Mansour Tarafdar <tarafdar.mansour@gmail.com>
2023-11-04 14:16:13 +0330

    feat(apigateway): upgrade apigateway to .net 7

--- ApiGateway/Ocelot/Startup.cs
+++ ApiGateway/Ocelot/Startup.cs
@@ -1,76 +1,67 @@
-﻿using Microsoft.AspNetCore.Builder;
+﻿using System.IO;
+using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Newtonsoft.Json;
 using Newtonsoft.Json.Linq;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 using Ocelot.Provider.Consul;
-using System;
-using System.Collections.Generic;
-using System.IO;
 
-namespace ApiGateway
+namespace ApiGateway;
+
+public class Startup
 {
-    public class Startup
+    private readonly IConfigurationRoot Configuration;
+
+    public Startup(IWebHostEnvironment env)
     {
-
-        private readonly IConfigurationRoot Configuration;
-
-        public Startup(IWebHostEnvironment env)
-        {
-            var ocelotJson = new JObject();
-            foreach (var jsonFilename in Directory.EnumerateFiles("Configuration", "ocelot.*.json", SearchOption.AllDirectories))
+        var ocelotJson = new JObject();
+        foreach (var jsonFilename in Directory.EnumerateFiles("Configuration", "ocelot.*.json",
+                     SearchOption.AllDirectories))
+            using (var fi = File.OpenText(jsonFilename))
             {
-                using (StreamReader fi = File.OpenText(jsonFilename))
+                var json = JObject.Parse(fi.ReadToEnd());
+                ocelotJson.Merge(json, new JsonMergeSettings
                 {
-                    var json = JObject.Parse(fi.ReadToEnd());
-                    ocelotJson.Merge(json, new JsonMergeSettings
-                    {
-                        MergeArrayHandling = MergeArrayHandling.Union
-                    });
-                }
+                    MergeArrayHandling = MergeArrayHandling.Union
+                });
             }
 
-            File.WriteAllText("ocelot.json", ocelotJson.ToString());
+        File.WriteAllText("ocelot.json", ocelotJson.ToString());
 
-            var builder = new ConfigurationBuilder()
-                .SetBasePath(env.ContentRootPath)
-                .AddJsonFile("ocelot.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"ocelot.{env.EnvironmentName}.json",
-                    optional: true, reloadOnChange: true)
-                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
-                .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
-                    optional: true, reloadOnChange: true)
-                .AddEnvironmentVariables();
+        var builder = new ConfigurationBuilder()
+            .SetBasePath(env.ContentRootPath)
+            .AddJsonFile("ocelot.json", false, true)
+            .AddJsonFile($"ocelot.{env.EnvironmentName}.json",
+                true, true)
+            .AddJsonFile("appsettings.json", false, true)
+            .AddJsonFile($"appsettings.{env.EnvironmentName}.json",
+                true, true)
+            .AddEnvironmentVariables();
 
-            Configuration = builder.Build();
-        }
+        Configuration = builder.Build();
+    }
 
-        // This method gets called by the runtime. Use this method to add services to the container.
-        public void ConfigureServices(IServiceCollection services)
-        {
-            services
-                .AddSwaggerForOcelot(Configuration)
-                .AddOcelot(Configuration)
-                .AddConsul()
-                .AddConfigStoredInConsul();
-        }
+    // This method gets called by the runtime. Use this method to add services to the container.
+    public void ConfigureServices(IServiceCollection services)
+    {
+        services
+            .AddSwaggerForOcelot(Configuration)
+            .AddOcelot(Configuration)
+            .AddConsul()
+            .AddConfigStoredInConsul();
+    }
 
-        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
-        public void Configure(IApplicationBuilder app)
-        {
-            app.UseStaticFiles();
+    // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+    public void Configure(IApplicationBuilder app)
+    {
+        app.UseStaticFiles();
 
-            app.UseSwaggerForOcelotUI(opt => {
-                opt.PathToSwaggerGenerator = "/swagger/docs";
-            });
+        app.UseSwaggerForOcelotUI(opt => { opt.PathToSwaggerGenerator = "/swagger/docs"; });
 
-            app
-                .UseOcelot()
-                .Wait();
-        }
+        app
+            .UseOcelot()
+            .Wait();
     }
 }
